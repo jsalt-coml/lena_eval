bd18f0a2965606d2a3885286eef98e749b10ffe4
MarvinLvn <marvin.lavechin@ensimag.grenoble-inp.fr>
2019-11-06 14:07:27 +0100

    Add chunks for expliciting how to use CTC/CVC files

--- scripts/extract_stat.r
+++ scripts/extract_stat.r
@@ -9,7 +9,7 @@ library(stringi)
 read_rttm <- function(data_folder) {
   all.files <- list.files(data_folder)
   # All rttm files excluding tsimane ones
-  rttm.files <- all.files[grep("*.rttm", all.files) & !startsWith(all.files, 'C') ]
+  rttm.files <- all.files[endsWith(all.files, '.rttm') & !startsWith(all.files, 'C') ]
   # Data structure where we'll store all the annotations
   data <- data.frame(filename = character(),
                      onset = double(),
@@ -23,12 +23,12 @@ read_rttm <- function(data_folder) {
     
     if (info['size'] != 0) {
       file_data = read.csv(file=filepath, header=FALSE, sep="\t")
-      file_data = file_data %>% select(2, 4, 5, 6, 7, 9) %>% rename(filename = V2,
+      file_data = file_data %>% select(2, 4, 5, 6, 7, 8) %>% rename(filename = V2,
                                        onset = V4,
                                        duration = V5,
                                        transcription = V6,
                                        utt_type = V7,
-                                       speaker_type = V9)
+                                       speaker_type = V8)
       
       file_data <- data.frame(file_data)
       data <- rbind(data, file_data)
@@ -43,7 +43,37 @@ read_rttm <- function(data_folder) {
   # And tier_subtype being a letter (N,C,L,Y...)
   data$tier_subtype = str_sub(data$utt_type,6,6)
   data$child_id = str_sub(data$filename,1,8)
+  data$end_time <- data$onset + data$duration
   data[data==""]<-NA
+  
+  ## Let's count turn-taking in the clearest way as possible
+  next_starts = c(data[2:nrow(data), "onset"], 10000)
+  prev_ends = data[1:nrow(data), "end_time"]
+  less_than_5 = (next_starts - prev_ends) < 5.0
+  less_than_5 = c(FALSE, less_than_5[1:length(less_than_5)-1])
+  data$less_than_5 = less_than_5
+  
+  change_files = c(data$filename,0) != c(0, data$filename)
+  change_files = change_files[1:length(change_files)-1]
+  data[change_files, "less_than_5"] = FALSE
+  
+  # How to map in R
+  gold_mapping.levels <- list(
+    OCH = c('C1', 'C2', 'FC1', 'MC1', 'MC2', 'MC3', 'MI1', 'UC1', 'UC2', 'UC3', 'UC4', 'UC5', 'UC6'),
+    FEM = c('FA1', 'FA2', 'FA3', 'FA4', 'FA5', 'FA6', 'FA7', 'FA8', 'MOT*'),
+    MAL = c('MA1','MA2', 'MA3', 'MA4', 'MA5'),
+    CHI = c('CHI','CHI*'),
+    ELE = c('EE1', 'FAE', 'MAE'))
+  data$mapped_speaker_type = data$speaker_type
+  levels(data$mapped_speaker_type) <- gold_mapping.levels
+  
+  prev_spkr = data[1:nrow(data), "mapped_speaker_type"]
+  next_spkr = factor(append(as.character(data[2:nrow(data), "mapped_speaker_type"]), "UNKUNK"))
+  cond1 = prev_spkr == 'CHI' & (next_spkr == 'MAL' | next_spkr == 'FEM')
+  cond2 = (prev_spkr == 'MAL' | prev_spkr == 'FEM') & next_spkr == 'CHI'
+  data$adult_chi_swipe = cond1 | cond2
+  data$turn_taking = data$adult_chi_swipe & data$less_than_5
+  data = data[,c(1,2,3,11,12,13,14,4,5,6,7,8,9,10)]
   return(data)
 }
 
@@ -70,6 +100,9 @@ get_stats <- function(data){
               CNV_mean = mean(duration, na.rm=TRUE), 
               CNV_count = length(duration))
   
+  child_CTC = data %>% group_by(child_id, age_mo_round) %>%
+    summarise(CTC_count = sum(turn_taking))
+  
   # File scale
   file_CVC = data %>% 
     filter(speaker_type == 'CHI', tier_type == 'vcm', tier_subtype == 'C' | tier_subtype == 'N') %>% 
@@ -85,6 +118,9 @@ get_stats <- function(data){
               CNV_mean = mean(duration, na.rm=TRUE), 
               CNV_count = length(duration))
   
+  file_CTC = data %>% group_by(filename, age_mo_round) %>%
+    summarise(CTC_count = sum(turn_taking))
+  
   # Aggregated across child and files
   all_CVC = data %>% 
     filter(speaker_type == 'CHI', tier_type == 'vcm', tier_subtype == 'C' | tier_subtype == 'N') %>%
@@ -97,10 +133,14 @@ get_stats <- function(data){
     summarise(CNV_cum_dur = sum(duration, na.rm=TRUE), 
               CNV_mean = mean(duration, na.rm=TRUE),
               CNV_count = length(duration))
-  
+  all_CTC = data %>% summarise(CTC_count = sum(turn_taking))
+
   child_stats = merge(child_CVC, child_CNVC)
+  child_stats = merge(child_stats, child_CTC)
   file_stats = merge(file_CVC, file_CNVC)
+  file_stats = merge(file_stats, file_CTC)
   all_stats = merge(all_CVC, all_CNVC)
+  all_stats = merge(all_stats, all_CTC)
   
   stats <- list()
   stats$child = child_stats
@@ -109,13 +149,11 @@ get_stats <- function(data){
   return(stats)
 }
 
-
 # Read the data
 # args = commandArgs(trailingOnly=TRUE)
 # if (length(args)!=1) {
 #   stop("The folder containg .rttm chunk files must be supplied", call.=FALSE)
 # } 
-
 gold_data_folder = "data/gold" #args[1]
 gold_data <- read_rttm(gold_data_folder)
 
@@ -132,4 +170,3 @@ output_folder = paste(getwd(), "evaluations", sep = "/")
 write.table(stats$child, file=paste(output_folder, "key_child_voc_child_level.csv", sep="/"), row.names=FALSE)
 write.table(stats$file, file=paste(output_folder, "key_child_voc_file_level.csv", sep="/"), row.names=FALSE)
 write.table(stats$all, file=paste(output_folder, "key_child_voc_corpora_level.csv", sep="/"), row.names=FALSE)
-
